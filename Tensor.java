b17a52dab7fcc23705da830f9345841fc199a646
RuimengY <162122658+RuimengY@users.noreply.github.com>
2024-04-18 12:32:47 +0800

    增加了stretch的功能，但似乎与pad之间有一点冲突，可以再修改

--- Tensor.java
+++ Tensor.java
@@ -143,6 +143,7 @@ public class Tensor {
         }
     }
 
+    // 对张量进行填充，在最后两个维度的位置扩展的大小设置为0
     public void pad(int padding) {
         // 在增加维度之前，建立一个数组，用来保存原来的data
         Object[] oldData = new Object[data.length];
@@ -204,9 +205,90 @@ public class Tensor {
         // this.data=result.data;
     }
 
+    // 拉伸：将一个张量的后两个维度拉伸到指定大小。
+    // 用拉伸前的像素值计算拉伸后的像素值，比如平均值
+    public void stretch(int height, int width) {
+        // 在增加维度之前，建立一个数组，用来保存原来的data
+        // 将data分为每个二维数组的值，方便求平均值
+        int times = data.length / (shape[shape.length - 1] * shape[shape.length - 2]);
+        int everySize = shape[shape.length - 1] * shape[shape.length - 2];
+        int[] average = new int[times];
+        System.out.println("average.length" + average.length);
+        Object[] oldData = new Object[data.length];
+        for (int i = 0; i < times; i++) {
+            int sum = 0;
+            for (int j = 0; j < everySize; j++) {
+                oldData[i * everySize + j] = data[i * everySize + j];
+                sum += (Integer) data[i * everySize + j];
+            }
+            average[i] = sum / everySize;
+            System.out.println("average[i]" + average[i]);
+        }
+        int paddingHeight = height / 2;
+        int paddingWidth = width / 2;
+        // 注意！认为高度在前，宽度在后
+        shape[shape.length - 2] = height;
+        shape[shape.length - 1] = width;
+        // 重新计算data的大小
+        data = new Object[calculateSize(this.shape)];
+        int dimensions = shape.length;
+        // 初始化，indices中的每个元素都是0
+        int[] indices = new int[dimensions];
+        // temp是保存每一次循环的所有索引，用来使用函数get()
+        int[] temp = new int[dimensions];
+        int sizeNow = height * width;
+        int count = 0;
+        int color = 0;
+        while (true) {
+            // 执行循环体操作,每次循环结束都是一个多维数组的索引
+            for (int i = 0; i < dimensions; i++) {
+                temp[i] = indices[i];
+            }
+            // 此时的temp中保存的是每一次循环的所有索引
+            // 开始判断位置，如果在padding的范围内，设置为0
+
+            if (indices[dimensions - 2] < paddingHeight
+                    || indices[dimensions - 2] >= shape[shape.length - 2] - paddingHeight
+                    || indices[dimensions - 1] < paddingWidth
+                    || indices[dimensions - 1] >= shape[dimensions - 1] - paddingWidth) {
+                // 每一张二维数组的周围都填充color的某个元素的值
+                this.set(average[color / sizeNow], temp);
+                // color++;现在的everysize是二维数组现在的大小，不是这个循环进行的大小
+            } else {
+                // 事实上，如果是原先的值，调用的顺序和原来是一样的
+
+                this.set(oldData[count], temp);
+                System.out.println("在范围内：" + get(temp));
+                count++;
+                // temp[dimensions - 2] -= padding;
+                // temp[dimensions - 1] -= padding;
+                // System.out.println("在范围外：" + temp[dimensions - 2] + " " + temp[dimensions -
+                // 1]);
+                // this.set(this.get(temp), indices);
+                // System.out.println(this.get(indices));
+
+            }
+            color++;// 此处进行++保证color的总次数是data的总次数
+            // 更新索引
+            // 确保从最后一维度开始改变索引
+            int j = dimensions - 1;
+            while (j >= 0 && indices[j] == shape[j] - 1) {
+                indices[j] = 0;
+                j--;
+            }
+            if (j < 0) {
+                break;
+            }
+            // 一开始都是0，将当前索引加1，直至值为loopCounts-1
+            indices[j]++;
+        }
+
+    }
+
     public static void main(String[] args) {
+
         Tensor tensor = new Tensor(2, 3);
-        tensor.set(10, 0, 0);
+        tensor.set(0, 0, 0);
         tensor.set(2, 0, 1);
         tensor.set(3, 0, 2);
         tensor.set(4, 1, 0);
@@ -231,6 +313,10 @@ public class Tensor {
         tensor.pad(2);
         System.out.println(tensor.get(0, 0));
         System.out.println(tensor.get(2, 2));
+
+        tensor.stretch(10, 10);
+        System.out.println(tensor.get(0, 0));
+        System.out.println(tensor.get(3, 3));
 
         /*
          * Tensor2 result3 = tensor.pad(2);
