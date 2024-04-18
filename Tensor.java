f79a5c1ce9e69560b589480df154f8b689b14559
RuimengY <162122658+RuimengY@users.noreply.github.com>
2024-04-18 10:39:48 +0800

    改好了数组原来元素修改以及昨天晚上整了一个晚上的错误的类里面套类的错误

--- Tensor.java
+++ Tensor.java
@@ -119,44 +119,37 @@ public class Tensor {
     }
 
     // 能够对张量进行索引操作，以获取特定的子张量。
-    public Tensor getSubTensorBySubDimensions(int... subDimensions) {
-        int[] subShape = new int[subDimensions.length];
-        for (int i = 0; i < subDimensions.length; i++) {
-            subShape[i] = shape[shape.length - subDimensions.length + i];
+    public Tensor getSubTensor(int... indices) {
+        int[] subShape = new int[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            subShape[i] = shape[indices[i]];
         }
         Tensor subTensor = new Tensor(subShape);
-        int[] subIndices = new int[subShape.length];
-
-        // 计算用shape的维度表示的subIndices
-        int[] totalDimensions = new int[shape.length];
-        for (int i = 0; i < shape.length; i++) {
-            totalDimensions[i] = i;
-        }
-        int[] indices = new int[shape.length];
-
+        int[] subIndices = new int[indices.length];
         for (int i = 0; i < data.length; i++) {
-            // 由i,shape和get方法计算indices
-            calculateSubIndicesBySubDimensions(i, totalDimensions, indices);
-            Object value = get(indices);
-
-            calculateSubIndicesBySubDimensions(i, subDimensions, subIndices);
-
+            calculateSubIndices(i, indices, subIndices);
+            Object value = get(subIndices);
             subTensor.set(value, subIndices);
-            // padding(subTensor, subIndices);
         }
         return subTensor;
     }
 
-    private void calculateSubIndicesBySubDimensions(int linearIndex, int[] subDimensions, int[] subIndices) {
+    private void calculateSubIndices(int linearIndex, int[] indices, int[] subIndices) {
         int remainingIndex = linearIndex;
-        for (int i = subDimensions.length - 1; i >= 0; i--) {
-            int dimSize = shape[shape.length - subDimensions.length + i];
+        for (int i = indices.length - 1; i >= 0; i--) {
+            int dimSize = shape[i];
             subIndices[i] = remainingIndex % dimSize;
             remainingIndex /= dimSize;
         }
     }
 
     public void pad(int padding) {
+        // 在增加维度之前，建立一个数组，用来保存原来的data
+        Object[] oldData = new Object[data.length];
+        for (int i = 0; i < oldData.length; i++) {
+            oldData[i] = data[i];
+        }
+
         // 将原来的shape的最后两个维度加上padding
         shape[shape.length - 1] = shape[shape.length - 1] + 2 * padding;
         shape[shape.length - 2] = shape[shape.length - 2] + 2 * padding;
@@ -168,6 +161,7 @@ public class Tensor {
         int[] indices = new int[dimensions];
         // temp是保存每一次循环的所有索引，用来使用函数get()
         int[] temp = new int[dimensions];
+        int count = 0;
         while (true) {
             // 执行循环体操作,每次循环结束都是一个多维数组的索引
             for (int i = 0; i < dimensions; i++) {
@@ -181,11 +175,18 @@ public class Tensor {
                     || indices[dimensions - 1] >= shape[dimensions - 1] - padding) {
                 this.set(0, temp);
             } else {
-                temp[dimensions - 2] -= padding;
-                temp[dimensions - 1] -= padding;
-                System.out.println("在范围外：" + temp[dimensions - 2] + " " + temp[dimensions - 1]);
-                this.set(this.get(temp), indices);
-                System.out.println(this.get(indices));
+                // 事实上，如果是原先的值，调用的顺序和原来是一样的
+
+                this.set(oldData[count], temp);
+                System.out.println("在范围内：" + get(temp));
+                count++;
+                // temp[dimensions - 2] -= padding;
+                // temp[dimensions - 1] -= padding;
+                // System.out.println("在范围外：" + temp[dimensions - 2] + " " + temp[dimensions -
+                // 1]);
+                // this.set(this.get(temp), indices);
+                // System.out.println(this.get(indices));
+
             }
             // 更新索引
             // 确保从最后一维度开始改变索引
@@ -203,93 +204,41 @@ public class Tensor {
         // this.data=result.data;
     }
 
-    // public Tensor2 pad(int padding) {
-    // int[] newShape = new int[2];
-    // 图片高度是shape[shape.length-2]，宽度是shape[shape.length-1]
-    // newShape[0] = shape[shape.length - 2] + 2 * padding;
-    // newShape[1] = shape[shape.length - 1] + 2 * padding;
-    // 建立新的二维的扩大的张量
-    // Tensor2 result = new Tensor2(newShape);
-    /*
-     * // indices的每个元素的初始值都是0(代表x轴和y轴)
-     * int[] indices = new int[2];
-     * // temp的每个元素的初始值都是0
-     * int[] temp = new int[2];
-     * while (true) {
-     * for (int i = 0; i < 2; i++) {
-     * temp[i] = indices[i];
-     * }
-     * if (indices[0] < padding || indices[0] >= shape[shape.length - 2] + padding
-     * || indices[1] < padding
-     * || indices[1] >= shape[shape.length - 1] + padding) {
-     * result.set(0, temp);
-     * } else {
-     * temp[0] -= padding;
-     * temp[1] -= padding;
-     * result.set(get(temp), indices);
-     * }
-     * int j = 1;
-     * while (j >= 0 && indices[j] == newShape[j] - 1) {
-     * indices[j] = 0;
-     * j--;
-     * }
-     * if (j < 0) {
-     * break;
-     * }
-     * indices[j]++;
-     * }
-     */
-    // return result;
-    // }
-
     public static void main(String[] args) {
-        Tensor tensor = new Tensor(2, 3, 1);
-        tensor.set(1, 0, 0, 0);
-        tensor.set(2, 0, 1, 0);
-        tensor.set(3, 0, 2, 0);
-        tensor.set(4, 1, 0, 0);
-        tensor.set(5, 1, 1, 0);
-        tensor.set(6, 1, 2, 0);
+        Tensor tensor = new Tensor(2, 3);
+        tensor.set(10, 0, 0);
+        tensor.set(2, 0, 1);
+        tensor.set(3, 0, 2);
+        tensor.set(4, 1, 0);
+        tensor.set(5, 1, 1);
+        tensor.set(6, 1, 2);
         Tensor other = new Tensor(2, 3);
         other.set(1, 0, 0);
         other.set(2, 0, 1);
         other.set(3, 0, 2);
-        other.set(7, 1, 0);
-        other.set(1, 1, 1);
+        other.set(4, 1, 0);
+        other.set(5, 1, 1);
         other.set(6, 1, 2);
+        Tensor result = tensor.add(other);
+
+        System.out.println(result.get(0, 0)); // Output: 1
+        System.out.println(result.get(1, 2)); // Output: 6
+
+        Tensor result2 = tensor.sub(other);
+        System.out.println(result2.get(0, 0)); // Output: 1
+        System.out.println(result2.get(1, 2)); // Output: 6
+
+        tensor.pad(2);
+        System.out.println(tensor.get(0, 0));
+        System.out.println(tensor.get(2, 2));
 
         /*
-         * Tensor subTensor = tensor.getSubTensorBySubDimensions(1, 2);
-         * // 输出子张量的形状
-         * System.out.println("SubTensor Shape: " + Arrays.toString(subTensor.shape));
-         * 
-         * // 输出子张量的数据
-         * for (int i = 0; i < subTensor.data.length; i++) {
-         * System.out.println("SubTensor Data at index " + i + ": " +
-         * subTensor.data[i]);
-         * }
-         * 
+         * Tensor2 result3 = tensor.pad(2);
+         * System.out.println(result3.get(0, 0)); // Output: 1
+         * System.out.println(result3.get(1, 2)); // Output: 6
+         * System.out.println(result3.get(0, 1)); // Output: 0
          */
-        tensor.pad(1);
-        System.out.println(tensor.get(0, 0, 0));
-        System.out.println(tensor.get(0, 1, 1));
+
     }
-
-    /*
-     * Tensor result = tensor.add(other);
-     * System.out.println(result.get(0, 0)); // Output: 1
-     * System.out.println(result.get(1, 2)); // Output: 6
-     * 
-     * Tensor result2 = tensor.sub(other);
-     * System.out.println(result2.get(0, 0)); // Output: 1
-     * System.out.println(result2.get(1, 2)); // Output : 6
-     */
-
-    /*
-     * Tensor2 result3 = tensor.pad(2);
-     * System.out.println(result3.get(0, 0)); // Output: 1
-     * System.out.println(result3.get(1, 2)); // Output: 6
-     * System.out.println(result3.get(0, 1)); // Output: 0
-     */
 
 }
