06010bb4063e733498a3eacb2cb2b05f8ff00525
RuimengY <162122658+RuimengY@users.noreply.github.com>
2024-04-19 12:08:11 +0800

    已经实现了卷积后的tensor类的result，现在差将result转为.txt形式和图片形式

--- Batch_test.java
+++ Batch_test.java
@@ -12,7 +12,7 @@ public class Batch_test {
     private Tensor kernel = new Tensor();
     private Tensor photoes = new Tensor();
 
-    public void getKernal() {
+    public void getKernelFromFile() {
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("sample_conv_kernel.txt"));
@@ -87,10 +87,50 @@ public class Batch_test {
         }
     }
 
+    // 实现kernel和photoes的卷积运算
+    public Tensor conv(int padding, int stride) {
+        // 将kernel进行padding处理
+        photoes.pad(padding);
+        // 计算卷积后的result的各个维数的大小
+        int Hout = (photoes.getShape(2) - kernel.getShape(2)) / stride + 1;
+        int Wout = (photoes.getShape(3) - kernel.getShape(3)) / stride + 1;
+        int num = photoes.getShape(0);
+        int channel = kernel.getShape(0);
+        Tensor result = new Tensor(num, channel, Hout, Wout);
+        for (int i = 0; i < num; i++) { // 遍历每张图片
+            for (int j = 0; j < channel; j++) { // 遍历每个输出通道
+                for (int m = 0; m < Hout; m++) {
+                    for (int n = 0; n < Wout; n++) {
+                        int sum = 0;
+                        for (int k = 0; k < 3; k++) { // 遍历每个输入通道(反正要红绿蓝三种颜色都加起来)
+                            // 将二维数组中的每个点都算出来
+                            for (int p = 0; p < kernel.getShape(2); p++) { // 遍历kernel的高
+                                for (int q = 0; q < kernel.getShape(3); q++) { // 遍历kernel的宽
+                                    int imgRow = m * stride + p;// 此时在photoes中的行数
+                                    int imgCol = n * stride + q;// 此时在photoes中的列数
+                                    if (imgRow < photoes.getShape(2) && imgCol < photoes.getShape(3)) {
+                                        // 由于此时已经确定了是四维数组，循环方便
+                                        sum += (int) photoes.get(i, k, imgRow, imgCol) * (int) kernel.get(j, k, p, q);
+                                    }
+                                }
+                            }
+                        }
+                        // 将三种颜色在该点的和作为result该点的值
+                        result.set(sum, i, j, m, n);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Batch_test test = new Batch_test();
-        test.getKernal();
+        test.getKernelFromFile();
         test.readImages();
+        Tensor result = test.conv(2, 1);
+        System.out.println(result.getShape(2));
+        System.out.println(result.get(0, 0, 0, 0));
     }
 
 }
